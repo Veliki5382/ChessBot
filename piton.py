c72a3272ef1816ff22842617a3661e14ec4563a2
Veliki5382 <kuzmanov03@gmail.com>
2023-12-09 02:48:50 +0100

    Update piton.py

--- piton.py
+++ piton.py
@@ -1,53 +1,126 @@
-import pygame as py
 import chess
-
-class Application:
-
-    def __init__(self):
-        py.init()
-        self.screen = py.display.set_mode((1600, 900))
-        self.clock = py.time.Clock()
-        self.is_running = True
-        
-        self.board = Board()
-    
-    def run(self):
-
-        while(self.is_running == True):
-            
-            for event in py.event.get():
-                if event.type == py.QUIT:
-                    self.is_running = False
-            
-            self.screen.fill("magenta")
-            self.board.draw_board(self.screen)
-
-            py.display.flip()
-
-        py.quit()
+#import sunfish
+import math
+import random
+import sys
 
 
-class Board:
 
-    def __init__(self):
-        self.light_color = (255, 228, 138)
-        self.dark_color = (133, 95, 30)
-        self.tile_size = 96
-        self.xOffset = 800 - 4 * self.tile_size
-        self.yOffset = 450 - 4 * self.tile_size
+def minimaxRoot(depth, board,isMaximizing):
+    possibleMoves = board.legal_moves
+    bestMove = -9999
+    secondBest = -9999
+    thirdBest = -9999
+    bestMoveFinal = None
+    for x in possibleMoves:
+        move = chess.Move.from_uci(str(x))
+        board.push(move)
+        value = max(bestMove, minimax(depth - 1, board, not isMaximizing))
+        board.pop()
+        if( value > bestMove):
+            print("Best score: " ,str(bestMove))
+            print("Best move: ",str(bestMoveFinal))
+            print("Second best: ", str(secondBest))
+            thirdBest = secondBest
+            secondBest = bestMove
+            bestMove = value
+            bestMoveFinal = move
+    return bestMoveFinal
 
-    def draw_board(self, screen):
-        
-        for i in range(8):
-            for j in range(8):
-                tile = py.Rect(self.xOffset+i*self.tile_size, self.yOffset+j*self.tile_size, self.tile_size, self.tile_size)
-                if i+j & 1:
-                    py.draw.rect(screen, self.dark_color, tile)
-                else:
-                    py.draw.rect(screen, self.light_color, tile)
+def minimax(depth, board, is_maximizing):
+    if(depth == 0):
+        return -evaluation(board)
+    possibleMoves = board.legal_moves
+    if(is_maximizing):
+        bestMove = -9999
+        for x in possibleMoves:
+            move = chess.Move.from_uci(str(x))
+            board.push(move)
+            bestMove = max(bestMove,minimax(depth - 1, board, not is_maximizing))
+            board.pop()
+        return bestMove
+    else:
+        bestMove = 9999
+        for x in possibleMoves:
+            move = chess.Move.from_uci(str(x))
+            board.push(move)
+            bestMove = min(bestMove, minimax(depth - 1, board, not is_maximizing))
+            board.pop()
+        return bestMove
+
+
+#def calculateMove(board):
+#    possible_moves = board.legal_moves
+#    if(len(possible_moves) == 0):
+#        print("No more possible moves...Game Over")
+#        sys.exit()
+#    bestMove = None
+#    bestValue = -9999
+#    n = 0
+#    for x in possible_moves:
+#        move = chess.Move.from_uci(str(x))
+#        board.push(move)
+#        boardValue = -evaluation(board)
+#        board.pop()
+#        if(boardValue > bestValue):
+#            bestValue = boardValue
+#            bestMove = move
+#
+#    return bestMove
+
+def evaluation(board):
+    i = 0
+    evaluation = 0
+    x = True
+    try:
+        x = bool(board.piece_at(i).color)
+    except AttributeError as e:
+        x = x
+    while i < 63:
+        i += 1
+        evaluation = evaluation + (getPieceValue(str(board.piece_at(i))) if x else -getPieceValue(str(board.piece_at(i))))
+    return evaluation
+
+
+def getPieceValue(piece):
+    if(piece == None):
+        return 0
+    value = 0
+    if piece == "P" or piece == "p":
+        value = 10
+    if piece == "N" or piece == "n":
+        value = 30
+    if piece == "B" or piece == "b":
+        value = 30
+    if piece == "R" or piece == "r":
+        value = 50
+    if piece == "Q" or piece == "q":
+        value = 90
+    if piece == 'K' or piece == 'k':
+        value = 900
+    #value = value if (board.piece_at(place)).color else -value
+    return value
+
+def main():
+    board = chess.Board()
+    n = 0
+    print(board)
+    while n < 100:
+        if n%2 == 0:
+            move = input("Enter move: ")
+            move = chess.Move.from_uci(str(move))
+            board.push(move)
+        else:
+            print("Computers Turn:")
+            move = minimaxRoot(4,board,True)
+            move = chess.Move.from_uci(str(move))
+            board.push(move)
+        print(board)
+        n += 1
+
+
+
 
 
 if __name__ == "__main__":
-    
-    app = Application()
-    app.run()
+    main()
